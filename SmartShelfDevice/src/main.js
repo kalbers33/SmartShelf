4b4a1b6bfdf3897cd97ffaa3ef11de149d65f761
kalbers33 <kevin.albers@berkeley.edu>
2015-04-11 15:22:37 -0700

    Added functionality to the newItem to return the index if a weight goes up since the request.

--- SmartShelfDevice/src/main.js
+++ SmartShelfDevice/src/main.js
@@ -9,6 +9,8 @@ var labelStyle = new Style({font:"bold 20px", color:"black"});
 
 var itemInformationObjects = [];
 var itemToAdd;
+var lastItemAdded = -1;
+var currentlyLookingForItem = false;
 
 var ItemInformation = function($){
 	this.name = $.name;
@@ -20,14 +22,21 @@ var ItemInformation = function($){
 	this.individualWeight = $.individualWeight;
 	this.totalWeight = 0;
 	this.maxWeight = 0;
+	this.lastWeight = this.totalWeight;
 	this.lowThreshold = 0.25; //25% item is low!
 	this.outThreshold = 0.1; //10% is essentially out (empty box)
 	this.status = "out";
 }
 
-ItemInformation.prototype.updateItemWeight = function(weight){
+ItemInformation.prototype.updateItemWeight = function(itemIndex, weight){
 	this.totalWeight = weight;
-	if(this.maxWeight < weight) this.maxWeight = weight;
+	if(this.maxWeight < weight){
+		this.maxWeight = weight;
+	}if(this.lastWeight < weight){
+		if(currentlyLookingForItem) lastItemAdded = itemIndex;
+		//TODO: Now Add the item!
+	}
+	this.lastWeight = weight;
 	this.count = Math.round(weight/this.individualWeight);
 }
 
@@ -43,7 +52,10 @@ Handler.bind("/getAllItemInformation", Behavior({
 Handler.bind("/newItem", Behavior({
 	onInvoke: function(handler, message){
 		itemToAdd = new ItemInformation(JSON.parse(message.requestText));
-		message.responseText = JSON.stringify({newShelf: -1});
+		message.responseText = JSON.stringify({newShelf: lastItemAdded});
+		if(lastItemAdded != -1) currentlyLookingForItem = false;
+		else currentlyLookingForItem = true;
+		lastItemAdded = -1;
 		message.status = 200;
 	}
 }));
@@ -127,12 +139,12 @@ var ApplicationBehavior = Behavior.template({
 		Shelves[5].string = data.Row2Column3;
 		
 		
-		itemInformationObjects[0].updateItemWeight(data.Row1Column1);
-		itemInformationObjects[1].updateItemWeight(data.Row1Column2);
-		itemInformationObjects[2].updateItemWeight(data.Row1Column3);
-		itemInformationObjects[3].updateItemWeight(data.Row2Column1);
-		itemInformationObjects[4].updateItemWeight(data.Row2Column2);
-		itemInformationObjects[5].updateItemWeight(data.Row2Column3); 
+		itemInformationObjects[0].updateItemWeight(0, data.Row1Column1);
+		itemInformationObjects[1].updateItemWeight(1, data.Row1Column2);
+		itemInformationObjects[2].updateItemWeight(2, data.Row1Column3);
+		itemInformationObjects[3].updateItemWeight(3, data.Row2Column1);
+		itemInformationObjects[4].updateItemWeight(4, data.Row2Column2);
+		itemInformationObjects[5].updateItemWeight(5, data.Row2Column3); 
 	}
 })
 
